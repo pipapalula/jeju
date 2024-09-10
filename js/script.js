169903b7660df988ffd204905980ef350e7c9ce9
pipapalula <demeter0470@gmail.com>
2024-09-10 10:26:12 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(".box").mouseover(function(){
+    $(this).find("video").get(0).play()
+})
+
+$(".box").mouseout(function(){
+    $(this).find("video").get(0).pause()
+})
+
+$(".box1").click(function(){
+    $(".page1").addClass("on")
+})
+
+$(".box2").click(function(){
+    $(".page2").addClass("on")
+})
+
+$(".box3").click(function(){
+    $(".page3").addClass("on")
+})
+
+$(".box4").click(function(){
+    $(".page4").addClass("on")
+})
+
+$(".close").click(function(){
+    $(".page").removeClass("on")
+})
